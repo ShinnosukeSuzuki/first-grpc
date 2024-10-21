3d1f66f7134fced2ce680e0512361b3a90944dc1
shinnosuke-suzuki <shinnosuke.suzuki@geniee.co.jp>
2024-10-22 01:06:18 +0900

    commit section 05

---
+++ cmd/server/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"os/signal"
+
+	hellopb "first-grpc/pkg/grpc"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+type myServer struct {
+	hellopb.UnimplementedGreetingServiceServer
+}
+
+// サービスメソッドの実装
+func (s *myServer) Hello(ctx context.Context, req *hellopb.HelloRequest) (*hellopb.HelloResponse, error) {
+	// リクエストからnameフィールドを取り出して
+	// "Hello, [名前]!"というレスポンスを返す
+	return &hellopb.HelloResponse{
+		Message: fmt.Sprintf("Hello, %s!", req.GetName()),
+	}, nil
+}
+
+// サービス構造体のコンストラクタを定義
+func NewMyServer() *myServer {
+	return &myServer{}
+}
+
+func main() {
+	// 8080番portのLisnterを作成
+	port := 8080
+	listener, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+	if err != nil {
+		panic(err)
+	}
+
+	// create a new gRPC server
+	s := grpc.NewServer()
+
+	// Register the service with the server
+	hellopb.RegisterGreetingServiceServer(s, NewMyServer())
+
+	// サーバーリフレクションの設定
+	reflection.Register(s)
+
+	// 作成したgRPCサーバーを、8080番ポートで稼働させる
+	go func() {
+		log.Printf("start gRPC server port: %v", port)
+		s.Serve(listener)
+	}()
+
+	// サーバーを停止するためのシグナルを受け取る
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+	log.Println("stopping gRPC server...")
+	s.GracefulStop()
+}
